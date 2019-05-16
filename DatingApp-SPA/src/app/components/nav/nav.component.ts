b1d1fc9d8f89af5397fcc12ff99ccf8589fa3889
cn935p <cn935p@att.com>
2019-05-15 20:34:00 -0500

    add authentication service with login functionality; add nav component; move value component

---
+++ DatingApp-SPA/src/app/components/nav/nav.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(private auth: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.auth.login(this.model).subscribe(next => {
+      console.log('Logged in successfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+  }
+
+}
