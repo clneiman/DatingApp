ec5cc52e3c73f130a51f9a7df49c2b7a646b50f6
cn935p <cn935p@att.com>
2019-05-16 19:26:50 -0500

    add home and register components; add register function to auth service; remove value component

---
+++ DatingApp-SPA/src/app/components/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { registerModuleFactory } from '@angular/core/src/linker/ng_module_factory_loader';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  registerMode: boolean;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
+
+}
